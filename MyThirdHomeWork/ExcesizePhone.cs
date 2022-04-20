925b73a23f3bf4d344a234ac97493d5604b98307
KrisjanisAboltins <krisjanis.aboltins@inbox.lv>
2022-04-20 22:23:51 +0300

    Add project files.

---
+++ MyThirdHomeWork/ExcesizePhone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyThirdHomeWork
+{
+    public class ExcesizePhone
+    {
+        public double Size { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public void Call()
+        {
+            Console.WriteLine("Calling");
+         }
+
+        public void Sms()
+        {
+            Console.WriteLine("SMS sent");
+        }
+
+
+    }
+}
+
+
+
---
+++ MyThirdHomeWork/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using MyThirdHomeWork;
+
+ExcesizePhone phone = new ExcesizePhone();
+
+Console.WriteLine($"pleaese write your phone size");
+string sizetext = Console.ReadLine();
+double size = double.Parse(sizetext);
+phone.Size = size;
+Console.WriteLine($"phone size is: {size}");
+
+string brand = Console.ReadLine();
+phone.Brand = brand;    
+
+string model = Console.ReadLine();
+phone.Model = model;
+
+
+
